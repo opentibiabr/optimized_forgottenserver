b797306e7b0553e0f65be368d653b9c476e0194c
SaiyansKing <38609240+SaiyansKing@users.noreply.github.com>
2020-05-18 23:43:58 +0200

    Change the way database tasks flush
    Change the way database tasks flush by making dispatcher wait until database thread do all queries instead of doing the queries in dispatcher thread.

--- src/databasetasks.cpp
+++ src/databasetasks.cpp
@@ -32,6 +32,9 @@ void DatabaseTasks::threadMain()
 	while (getState() != THREAD_STATE_TERMINATED) {
 		taskLockUnique.lock();
 		if (tasks.empty()) {
+			if (flushTasks) {
+				flushSignal.notify_one();
+			}
 			taskSignal.wait(taskLockUnique);
 		}
 
@@ -83,20 +86,16 @@ void DatabaseTasks::runTask(const DatabaseTask& task)
 void DatabaseTasks::flush()
 {
 	std::unique_lock<std::mutex> guard{ taskLock };
-	while (!tasks.empty()) {
-		auto task = std::move(tasks.front());
-		tasks.pop_front();
-		guard.unlock();
-		runTask(task);
-		guard.lock();
+	if (!tasks.empty()) {
+		flushTasks = true;
+		flushSignal.wait(guard);
+		flushTasks = false;
 	}
 }
 
 void DatabaseTasks::shutdown()
 {
-	taskLock.lock();
-	setState(THREAD_STATE_TERMINATED);
-	taskLock.unlock();
 	flush();
+	setState(THREAD_STATE_TERMINATED);
 	taskSignal.notify_one();
 }
--- src/databasetasks.h
+++ src/databasetasks.h
@@ -52,6 +52,8 @@ class DatabaseTasks : public ThreadHolder<DatabaseTasks>
 		std::list<DatabaseTask> tasks;
 		std::mutex taskLock;
 		std::condition_variable taskSignal;
+		std::condition_variable flushSignal;
+		bool flushTasks = false;
 };
 
 extern DatabaseTasks g_databaseTasks;
