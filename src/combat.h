88548b3e6ccdb62c8abfa1da1525e97e81d27d44
SaiyansKing <38609240+SaiyansKing@users.noreply.github.com>
2020-06-10 02:05:51 +0200

    Refactoring combats
    -Use blockprotile tile caching
    -Merge checking tilestate's
    -Keep MatrixArea in-house of AreaCombat class

--- src/combat.h
+++ src/combat.h
@@ -82,51 +82,53 @@ using CombatFunction = std::function<void(Creature*, Creature*, const CombatPara
 class MatrixArea
 {
 	public:
-		MatrixArea(uint32_t rows, uint32_t cols): centerX(0), centerY(0), rows(rows), cols(cols) {
-			data_ = new bool*[rows];
+		typedef std::conditional<8 < sizeof(size_t), uint32_t, uint64_t>::type _Ty;
+		enum : ptrdiff_t {
+			_Bitsperword = static_cast<ptrdiff_t>(CHAR_BIT * sizeof(_Ty)),
+		};
 
-			for (uint32_t row = 0; row < rows; ++row) {
-				data_[row] = new bool[cols];
+		MatrixArea() = default;
 
-				for (uint32_t col = 0; col < cols; ++col) {
-					data_[row][col] = 0;
-				}
-			}
-		}
+		// non-copyable
+		MatrixArea(const MatrixArea&) = delete;
+		MatrixArea& operator=(const MatrixArea&) = delete;
 
-		MatrixArea(const MatrixArea& rhs) {
-			centerX = rhs.centerX;
-			centerY = rhs.centerY;
-			rows = rhs.rows;
-			cols = rhs.cols;
-
-			data_ = new bool*[rows];
-
-			for (uint32_t row = 0; row < rows; ++row) {
-				data_[row] = new bool[cols];
-
-				for (uint32_t col = 0; col < cols; ++col) {
-					data_[row][col] = rhs.data_[row][col];
-				}
-			}
-		}
+		// non-moveable
+		MatrixArea(const MatrixArea&&) = delete;
+		MatrixArea& operator=(const MatrixArea&&) = delete;
 
 		~MatrixArea() {
-			for (uint32_t row = 0; row < rows; ++row) {
-				delete[] data_[row];
-			}
-
 			delete[] data_;
 		}
 
-		// non-assignable
-		MatrixArea& operator=(const MatrixArea&) = delete;
+		void setupArea(uint32_t rows, uint32_t cols) {
+			delete[] data_;
+
+			this->centerX = 0;
+			this->centerY = 0;
+			this->rows = rows;
+			this->cols = cols;
+			data_ = new _Ty[(((rows * cols) - 1) / _Bitsperword) + 1];
+			for (uint32_t i = 0; i < (rows * cols); i += _Bitsperword) {
+				data_[i / _Bitsperword] = 0;
+			}
+		}
+		void clear() {
+			delete[] data_;
+			data_ = nullptr;
+		}
 
 		void setValue(uint32_t row, uint32_t col, bool value) const {
-			data_[row][col] = value;
+			uint32_t index = (row * cols) + col;
+			if (value) {
+				data_[index / _Bitsperword] |= (static_cast<_Ty>(1) << (index % _Bitsperword));
+			} else {
+				data_[index / _Bitsperword] &= ~(static_cast<_Ty>(1) << (index % _Bitsperword));
+			}
 		}
 		bool getValue(uint32_t row, uint32_t col) const {
-			return data_[row][col];
+			uint32_t index = (row * cols) + col;
+			return ((data_[index / _Bitsperword] & (static_cast<_Ty>(1) << (index % _Bitsperword))) != 0);
 		}
 
 		void setCenter(uint32_t y, uint32_t x) {
@@ -145,20 +147,18 @@ class MatrixArea
 			return cols;
 		}
 
-		const bool* operator[](uint32_t i) const {
-			return data_[i];
-		}
-		bool* operator[](uint32_t i) {
-			return data_[i];
+		bool isInitialized() const {
+			return data_;
 		}
 
 	private:
+		_Ty* data_ = nullptr; // It would actually be great if we can have that in-house but we don't know how much data we'll need
+
 		uint32_t centerX;
 		uint32_t centerY;
 
 		uint32_t rows;
 		uint32_t cols;
-		bool** data_;
 };
 
 class AreaCombat
@@ -167,9 +167,6 @@ class AreaCombat
 		AreaCombat() = default;
 
 		AreaCombat(const AreaCombat& rhs);
-		~AreaCombat() {
-			clear();
-		}
 
 		// non-assignable
 		AreaCombat& operator=(const AreaCombat&) = delete;
@@ -192,7 +189,7 @@ class AreaCombat
 			MATRIXOPERATION_ROTATE270,
 		};
 
-		MatrixArea* createArea(const std::list<uint32_t>& list, uint32_t rows);
+		MatrixArea* createArea(Direction dir, const std::list<uint32_t>& list, uint32_t rows);
 		static void copyArea(const MatrixArea* input, MatrixArea* output, MatrixOperation_t op);
 
 		MatrixArea* getArea(const Position& centerPos, const Position& targetPos) const {
@@ -222,14 +219,14 @@ class AreaCombat
 				}
 			}
 
-			auto it = areas.find(dir);
-			if (it == areas.end()) {
-				return nullptr;
+			const MatrixArea& area = areas[dir];
+			if (area.isInitialized()) {
+				return const_cast<MatrixArea*>(&area);
 			}
-			return it->second;
+			return nullptr;
 		}
 
-		std::map<Direction, MatrixArea*> areas;
+		MatrixArea areas[DIRECTION_LAST + 1];
 		bool hasExtArea = false;
 };
 
