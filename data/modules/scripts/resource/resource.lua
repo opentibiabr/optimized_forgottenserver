b07fe4b2e15add0cfca25f5482cf45ae421b73bb
SaiyansKing <38609240+SaiyansKing@users.noreply.github.com>
2020-09-01 03:33:04 +0200

    Fix sending resource types

--- data/modules/scripts/resource/resource.lua
+++ data/modules/scripts/resource/resource.lua
@@ -11,7 +11,7 @@ local function sendResource(player, resourceType, resourceValue)
 	local omsg = NetworkMessage()
 	omsg:addByte(0xEE)
 	omsg:addByte(resourceType)
-	if(resourceType == RESOURCETYPE_CHARM_POINTS) then
+	if(resourceType == ResourceConfig.RESOURCETYPE_CHARM_POINTS) then
 		omsg:addU32(resourceValue)
 	else
 		omsg:addU64(resourceValue)
@@ -22,17 +22,17 @@ end
 
 function onRecvbyte(player, msg, byte)
 	local resourceType = msg:getByte()
-	if(resourceType == RESOURCETYPE_BANK_GOLD) then
-		sendResource(player, RESOURCETYPE_BANK_GOLD, player:getBankBalance())
-	elseif(resourceType == RESOURCETYPE_INVENTORY_GOLD) then
-		sendResource(player, RESOURCETYPE_INVENTORY_GOLD, player:getMoney())
-	elseif(resourceType == RESOURCETYPE_PREY_BONUS_REROLLS) then
-		sendResource(player, RESOURCETYPE_PREY_BONUS_REROLLS, 0)
-	elseif(resourceType == RESOURCETYPE_COLLECTION_TOKENS) then
-		sendResource(player, RESOURCETYPE_COLLECTION_TOKENS, 0)
-	elseif(resourceType == RESOURCETYPE_CHARM_POINTS) then
-		sendResource(player, RESOURCETYPE_CHARM_POINTS, 0)
-	elseif(resourceType == RESOURCETYPE_TOURNAMENT_TICKET_VOUCHERS) then
-		sendResource(player, RESOURCETYPE_TOURNAMENT_TICKET_VOUCHERS, 0)
+	if(resourceType == ResourceConfig.RESOURCETYPE_BANK_GOLD) then
+		sendResource(player, ResourceConfig.RESOURCETYPE_BANK_GOLD, player:getBankBalance())
+	elseif(resourceType == ResourceConfig.RESOURCETYPE_INVENTORY_GOLD) then
+		sendResource(player, ResourceConfig.RESOURCETYPE_INVENTORY_GOLD, player:getMoney())
+	elseif(resourceType == ResourceConfig.RESOURCETYPE_PREY_BONUS_REROLLS) then
+		sendResource(player, ResourceConfig.RESOURCETYPE_PREY_BONUS_REROLLS, 0)
+	elseif(resourceType == ResourceConfig.RESOURCETYPE_COLLECTION_TOKENS) then
+		sendResource(player, ResourceConfig.RESOURCETYPE_COLLECTION_TOKENS, 0)
+	elseif(resourceType == ResourceConfig.RESOURCETYPE_CHARM_POINTS) then
+		sendResource(player, ResourceConfig.RESOURCETYPE_CHARM_POINTS, 0)
+	elseif(resourceType == ResourceConfig.RESOURCETYPE_TOURNAMENT_TICKET_VOUCHERS) then
+		sendResource(player, ResourceConfig.RESOURCETYPE_TOURNAMENT_TICKET_VOUCHERS, 0)
 	end
 end
