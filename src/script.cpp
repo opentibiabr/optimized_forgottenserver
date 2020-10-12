84c2a93b6e7dfe8e7d3e93d8aec2674220e64569
SaiyansKing <38609240+SaiyansKing@users.noreply.github.com>
2020-10-13 01:30:30 +0200

    Remove experimental filesystem

--- src/script.cpp
+++ src/script.cpp
@@ -25,9 +25,6 @@
 #ifdef __cpp_lib_filesystem
 #include <filesystem>
 namespace fs = std::filesystem;
-#elif __cpp_lib_experimental_filesystem
-#include <experimental/filesystem>
-namespace fs = std::experimental::filesystem;
 #else
 #include <boost/filesystem.hpp>
 namespace fs = boost::filesystem;
